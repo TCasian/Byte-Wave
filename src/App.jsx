076ea9f81c0058e6b9041099d0ad96db8e98d99c
Casian <mitocarucasian@protonmail.com>
2025-11-06 18:47:45 +0100

    test google 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,53 @@
-
-import './App.css'
-import Header from './componenti/Header.jsx'
-import { useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { supabase } from './supabaseClient.js'
-import Security from './pages/security/security'
-import AddArticles from './pages/add-articles/add-articles';
-import Login from './pages/auth/login'
-import Signup from './pages/auth/signup'
+import "./App.css";
+import Header from "./componenti/Header.jsx";
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { supabase } from "./supabaseClient.js";
+import Security from "./pages/security/security";
+import AddArticles from "./pages/add-articles/add-articles";
+import Login from "./pages/auth/login";
+import Signup from "./pages/auth/signup";
 
 function App() {
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      console.log("⚡ Evento auth:", event);
+      if (session?.user) {
+        console.log("🔐 Loggato:", session.user);
+      } else {
+        console.log("🚪 Logout o sessione scaduta");
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={
-          <>
-            <div className="top-headlines">
-              <div className='titles'></div>
-              <div className='articles'></div>
-            </div>
-            <div className="ad-space"></div>
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="top-headlines">
+                <div className="titles"></div>
+                <div className="articles"></div>
+              </div>
+              <div className="ad-space"></div>
+            </>
+          }
+        />
 
         <Route path="/security" element={<Security />} />
         <Route path="/add-articles" element={<AddArticles />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
